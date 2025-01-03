0f12b3d2aedcb07576ec6c7b80094bd83fc12077
elpassa339gmail.com <elpassa339@gmail.com>
2025-01-03 16:10:04 +0100

    Mise en place de la base

--- views/templates/base.php
+++ views/templates/base.php
@@ -15,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tom Troc - <?= $title ?></title>
     <link rel="icon" type="image/x-icon" href="images/icone.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
