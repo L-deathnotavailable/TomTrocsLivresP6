8703393acc3a04c160b98cade1227f38f7f905f3
elpassa339gmail.com <elpassa339@gmail.com>
2025-01-03 16:09:47 +0100

    Mise en place du footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,10 +1,8 @@
-
-</main>
 <footer>
-    <p>Politique de confidentialité</p>
-    <p>Mentions légales</p>
-    <p>&copy; 2024 TomTroc</p>
-    <!-- logo TomTroc à mettre ici -->
+    <a href="index.php" class="footer-link">Politique de confidentialité</a>
+    <a href="index.php" class="footer-link">Mentions légales</a>
+    <a href="index.php" class="footer-link">Tom Troc©</a>
+    <a href="index.php" class="footer-link">
+        <img src="images/logo-footer.png" alt="Logo Tom troc footer" class="footer-img">
+    </a>
 </footer>
-</body>
-</html>
