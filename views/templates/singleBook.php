edb053fbefea1175917d8dbd9b51608355aafbd7
elpassa339gmail.com <elpassa339@gmail.com>
2025-01-13 16:21:12 +0100

    Ajout de la page SingleBook static

---
+++ views/templates/singleBook.php
@@ -0,0 +1,36 @@
+<section class="SingleBook_details">
+
+    <div class="book-details-illustration">
+        <img src="books/images/TheKinfolkTable.png" alt="title Book">
+    </div>
+
+    <div class="book-details-text">
+        <div class="header-book-details-text">
+            <h2 class="book-details-title">The Kinkfolk Table</h2>
+            <p class="book-details-author">par Nathan Williams</p>
+            <div class="book-details-separator">___</div>
+        </div>
+
+        <div class="book-details-description">
+            <p class="book-details-subtitle">DESCRIPTION</p>
+            <p class="book-details-description-text">J'ai récemment plongé dans les pages de 'The Kinfolk Table' et j'ai été enchanté par cette œuvre captivante. 
+                Ce livre va bien au-delà d'une simple collection de recettes ; il célèbre l'art de partager des moments authentiques autour de la table. 
+                Les photographies magnifiques et le ton chaleureux captivent dès le départ, transportant le lecteur dans un voyage à travers des recettes 
+                et des histoires qui mettent en avant la beauté de la simplicité et de la convivialité. 
+                Chaque page est une invitation à ralentir, à savourer et à créer des souvenirs durables avec les êtres chers. 
+                'The Kinfolk Table' incarne parfaitement l'esprit de la cuisine et de la camaraderie, et il est certain que ce livre trouvera une place 
+                spéciale dans le cœur de tout amoureux de la cuisine et des rencontres inspirantes.</p>
+        </div>
+
+        <div class="book-details-owner">
+            <p class="book-details-subtitle">PROPRIÉTAIRE</p>
+            <a class="book-details-owner-photo" href="#">
+                <img src="UserImages/Alexlecture.png" alt="Alexlecture user image">
+                <p>Alexlecture</p>
+            </a>
+        </div>
+        <details class="book-details-contact">
+            <summary><div class="green-button">Envoyer un message</div></summary>
+        </details>
+    </div>
+</section>
